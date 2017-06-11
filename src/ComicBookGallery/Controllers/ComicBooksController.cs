d78aa9f4b0c07092b7c5da8084c4a14df1d0437d
Brandy Snider <bsnider86@gmail.com>
2017-06-10 21:46:12 -0400

    Added comic books controller

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -7,10 +7,9 @@ using System.Web.Mvc;
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
-    {
-        public string Detail()
+  
+        
         {
-            return "Hello from the comic books controller!"
+           
         }
     }
-}
